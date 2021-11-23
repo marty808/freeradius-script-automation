d44d747377410c055e78e46a6006e969a47e55a8
marty808 <helge@labbifant.de>
2021-11-23 16:42:27 +0100

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+FROM freeradius/freeradius-server
+
+ENV CRON_SCHEDULE="*/1 * * * *"
+
+# install packages
+RUN apt-get update && \
+    apt-get install -y python3 python3-openpyxl cron
+
+
+# change files
+RUN mkdir -p /config/exec.d/ && \
+    sed -i 's/auth = no$/auth = yes/' /etc/freeradius/radiusd.conf && \
+    echo "# include config file\n\$INCLUDE /config/clients.conf\n" >> /etc/freeradius/clients.conf && \
+    echo "# include config file\n\$INCLUDE /config/huntgroups\n" >> /etc/freeradius/huntgroups && \
+    sed -i '1i# include config file\n$INCLUDE /config/users\n' /etc/freeradius/users
+
+
+COPY docker-entrypoint.sh /docker-entrypoint.sh
+RUN chmod +x /docker-entrypoint.sh    
+ENTRYPOINT ["/docker-entrypoint.sh"]
+
+
+
---
+++ docker-entrypoint.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+if [[ ! -d "/config/exec.d/" ]]; then
+  mkdir -p /config/exec.d
+fi
+
+if [[ ! -f "/config/clients.conf" ]]; then
+  echo "# don't delete this file" > /config/clients.conf
+fi
+
+if [[ ! -f "/config/huntgroups" ]]; then
+  echo "# don't delete this file" > /config/huntgroups
+fi
+
+
+if [[ ! -f "/config/users" ]]; then
+  echo "# don't delete this file" > /config/users
+fi
+
+
+echo "$CRON_SCHEDULE    root    run-parts /config/exec.d >> /var/log/script.log" > /etc/cron.d/script
+
+touch /var/log/script.log
+
+# start executables
+echo "starting cron..."
+cron
+
+freeradius -C
+if [[ $? -ne 0 ]]; then 
+  echo "$(date) : freeradius config check failed!!"
+  exit 1
+else 
+  echo "freeradius config check passed..."
+  echo "starting freeradius..."
+  freeradius
+fi
+
+
+# log to stdout
+tail -f /var/log/freeradius/radius.log &
+tail -f /var/log/script.log
+
+
